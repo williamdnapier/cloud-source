5435cedbecdadb04745ad8ff01c60862e90475b5
williamdnapier <napier.bill@gmail.com>
2021-10-02 09:15:04 -0400

    update app.vue, main.js; add components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <store />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Store from "./components/Store";
+import { mapActions } from "vuex";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  components: { Store },
+  methods: {
+    ...mapActions(["getData"])
+  },
+  created() {
+    this.getData();
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
