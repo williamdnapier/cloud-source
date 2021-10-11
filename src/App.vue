4f0a93b7eaf5cc65b875ebe8a3541fe972ee671d
williamdnapier <napier.bill@gmail.com>
2021-10-11 08:24:37 -0400

    remove store, add router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <store />
+  <router-view />
 </template>
 
 <script>
-/*
-To run this app:
-
-1. Open a terminal session and enter:
-npm run json
-This will start the json-server to act as our backend RESTful service.
-
-2. Open another terminal session and enter:
-npm run serve
-This will run our Vue.js UI.
-*/
-
-import Store from "./components/Store";
+//import Store from "./components/Store";
 import { mapActions } from "vuex";
 
 export default {
   name: 'app',
-  components: { Store },
+  //components: { Store },
   methods: {
     ...mapActions(["getData"])
   },
@@ -29,3 +17,15 @@ export default {
   }
 }
 </script>
+
+/*
+To run this app:
+
+1. Open a terminal session and enter:
+npm run json
+This starts json-server.
+
+2. Open another terminal session and enter:
+npm run serve
+This will run our Vue.js UI.
+*/
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import store from './store';
+import router from './router'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+  store,
+  router
+}).$mount('#app')
+
 /*
 Adding the bootstrap and font-awesome packages for use in the application.
 Using import statements to grab the min.css files which were added via mpm install.
@@ -7,12 +20,3 @@ i.e.
 npm install bootstrap@4.0.0
 npm install font-awesome@4.7.0
  */
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-import store from './store'
-
-Vue.config.productionTip = false
-new Vue({
-  render: h => h(App),
-  store
-}).$mount('#app')
